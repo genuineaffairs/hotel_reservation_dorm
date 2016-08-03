8f27d8a3dbd44edc479a775f1db808c74de91c26
hallos <oscar.hall@outlook.com>
2016-08-03 12:04:01 -0400

    Adding module skeleton

---
+++ __openerp__.py
@@ -0,0 +1,9 @@
+{
+	'name': 'Hotel Dormitoy Reservation',
+	'description': 'Extends Hotel Managment suite to allow reservations in dormitories.',
+	'author' : 'Oscar Hall',
+	'depends' : ['hotel_reservation','hotel_room'],
+	'data' : [				
+				'views/hotel_reservation_view.xml',
+				'views/hotel_reservation_workflow.xml',],
+}
---
+++ models/hotel_reservation.py
@@ -0,0 +1,7 @@
+from openerp import models, fields, api
+
+class HotelReservation(models.Model):
+	_inherit = 'hotel.reservation'
+
+	
+							
