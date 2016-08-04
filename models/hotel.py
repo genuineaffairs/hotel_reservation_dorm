bbd6e67b5a80780fc01685f79bf2cfe668bc2784
hallos <oscar.hall@outlook.com>
2016-08-04 10:24:33 -0400

    Add bed-model to hotel managment

--- models/hotel.py
+++ models/hotel.py
@@ -4,5 +4,13 @@ class HotelRoom(models.Model):
 	_inherit = 'hotel.room'
 	
 	dormitory = fields.Boolean('Dormitory')
+	bed_ids = fields.One2many('hotel.room.bed')
+	
 
+class HotelBed(models.Model):
+	_name = 'hotel.room.bed'
+	_inherit = 'hotel.room'
+	
+	name = fields.Char('Bed name') 
+	room_id = fields.Many2one('hotel.room')
 	
