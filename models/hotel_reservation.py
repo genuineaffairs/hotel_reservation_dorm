207940513aee4198bc7e019f88912ae28c11cf97
hallos <oscar.hall@outlook.com>
2016-08-04 16:28:58 -0400

    Printing bed_ids

--- models/hotel_reservation.py
+++ models/hotel_reservation.py
@@ -52,6 +52,10 @@ class HotelReservation(models.Model):
 					print line_id
 					line_id = line_id.reserve
 					for room_id in line_id:
+						print "ROOM ID"
+						print room_id
+						for bed in room_id.bed_ids
+							print bed
 						vals = {
 							'room_id': room_id.id,
 							'check_in': reservation.checkin,
