06a3a0ec7ee1f92e98ec4d4ebc8a25ec031e93ee
hallos <oscar.hall@outlook.com>
2016-08-04 14:10:08 -0400

    Add reservline capaciy to bed

--- models/hotel.py
+++ models/hotel.py
@@ -5,6 +5,7 @@ class HotelRoom(models.Model):
 	
 	dormitory = fields.Boolean('Dormitory')
 	bed_ids = fields.One2many('hotel.room.bed','room_id')
+	nr_beds = fields.Integer('Nr of beds in room', default=1)
 	
 
 class HotelBed(models.Model):
@@ -12,5 +13,11 @@ class HotelBed(models.Model):
 	
 	name = fields.Char('Bed name') 
 	room_id = fields.Many2one('hotel.room','bed_ids')
+	
+	status = fields.Selection([('available', 'Available'),
+							('occupied', 'Occupied')],
+							'Status', default='available')
+	capacity = fields.Integer('Capacity', default=1)
+	bed_line_ids = fields.One2many('folio.room.line', 'room_id', string='Bed Reservation Line')
 	
 	
