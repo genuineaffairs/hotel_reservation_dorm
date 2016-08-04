071ed24dd8fcbd685c4a057ac8f730b84b98e724
hallos <oscar.hall@outlook.com>
2016-08-04 16:19:40 -0400

    Modyfing reservation.line

--- models/hotel.py
+++ models/hotel.py
@@ -12,7 +12,7 @@ class HotelBed(models.Model):
 	_name = 'hotel.room.bed'
 	
 	name = fields.Char('Bed name') 
-	room_id = fields.Many2one('hotel.room','bed_ids')
+	room_id = fields.Many2one('hotel.room','bed_ids', string='In Room')
 	
 	status = fields.Selection([('available', 'Available'),
 							('occupied', 'Occupied')],
