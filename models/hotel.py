808674154a467ee924c31e28f5c40f18d26b5c6c
hallos <oscar.hall@outlook.com>
2016-08-10 17:02:18 -0400

    Multiple qty of room when creating folio of a dorm

--- models/hotel.py
+++ models/hotel.py
@@ -65,9 +65,11 @@ class HotelFolio(models.Model):
 	def folio_room_lines(self):
 		folio_rooms = []
 		for room in self[0].room_lines:
-			room_obj = self.env['hotel.room'].search([('product_id','=',room.product_id.id)])
-			print room_obj.name
 			if room.product_id.id in folio_rooms:
 				raise exceptions.Warning('You cannot take same room twice')
-			folio_rooms.append(room.product_id.id)
+			# Get a handle on the room the reservation regards
+			room_obj = self.env['hotel.room'].search([('product_id','=',room.product_id.id)])
+			# If the room is a dormitory we will allow to make multiple folio.lines
+			if room_obj.dormitory:
+				folio_rooms.append(room.product_id.id)
 			
