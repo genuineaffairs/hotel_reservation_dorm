5aba22242651f7143a5a7772b402f097041f6f31
hallos <oscar.hall@outlook.com>
2016-08-10 10:23:07 -0400

    Hide add an-item option in hotelroom bed-view

--- __openerp__.py
+++ __openerp__.py
@@ -1,6 +1,6 @@
 {
 	'name': 'Hotel Dormitory Reservation',
-	'description': 'Extends Hotel Managment suite to allow reservations in dormitories.',
+	'description': 'Extends Hotel Managment suite to allow reservations on dorms.',
 	'author' : 'Oscar Hall',
 	'depends' : ['hotel_reservation','hotel'],
 	'data' : [
