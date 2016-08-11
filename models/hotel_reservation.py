102590db3fe2be09221954c43b080099bd087646
hallos <oscar.hall@outlook.com>
2016-08-11 10:05:34 -0400

    Dorm-functionality in room.reservation.summary

--- models/hotel_reservation.py
+++ models/hotel_reservation.py
@@ -188,98 +188,98 @@ class RoomReservationSummary(models.Model):
 	_inherit = 'room.reservation.summary'
 	
 	# Override get_room_summary to include dorm-functionality
-    @api.onchange('date_from', 'date_to')
-    def get_room_summary(self):
-        '''
-        @param self: object pointer
-         '''
-        res = {}
-        all_detail = []
-        room_obj = self.env['hotel.room']
-        reservation_line_obj = self.env['hotel.room.reservation.line']
-        folio_room_line_obj = self.env['folio.room.line']
-        date_range_list = []
-        main_header = []
-        summary_header_list = ['Rooms']
-        if self.date_from and self.date_to:
-            if self.date_from > self.date_to:
-                raise except_orm(_('User Error!'),
-                                 _('Please Check Time period Date \
-                                 From can\'t be greater than Date To !'))
-            d_frm_obj = (datetime.datetime.strptime
-                         (self.date_from, DEFAULT_SERVER_DATETIME_FORMAT))
-            d_to_obj = (datetime.datetime.strptime
-                        (self.date_to, DEFAULT_SERVER_DATETIME_FORMAT))
-            temp_date = d_frm_obj
-            while(temp_date <= d_to_obj):
-                val = ''
-                val = (str(temp_date.strftime("%a")) + ' ' +
-                       str(temp_date.strftime("%b")) + ' ' +
-                       str(temp_date.strftime("%d")))
-                summary_header_list.append(val)
-                date_range_list.append(temp_date.strftime
-                                       (DEFAULT_SERVER_DATETIME_FORMAT))
-                temp_date = temp_date + datetime.timedelta(days=1)
-            all_detail.append(summary_header_list)
-            room_ids = room_obj.search([])
-            all_room_detail = []
-            for room in room_ids:
-                room_detail = {}
-                room_list_stats = []
-                room_detail.update({'name': room.name or ''})
+	@api.onchange('date_from', 'date_to')
+		def get_room_summary(self):
+		'''
+		@param self: object pointer
+		 '''
+		res = {}
+		all_detail = []
+		room_obj = self.env['hotel.room']
+		reservation_line_obj = self.env['hotel.room.reservation.line']
+		folio_room_line_obj = self.env['folio.room.line']
+		date_range_list = []
+		main_header = []
+		summary_header_list = ['Rooms']
+		if self.date_from and self.date_to:
+			if self.date_from > self.date_to:
+				raise except_orm(_('User Error!'),
+								 _('Please Check Time period Date \
+								 From can\'t be greater than Date To !'))
+			d_frm_obj = (datetime.datetime.strptime
+						 (self.date_from, DEFAULT_SERVER_DATETIME_FORMAT))
+			d_to_obj = (datetime.datetime.strptime
+						(self.date_to, DEFAULT_SERVER_DATETIME_FORMAT))
+			temp_date = d_frm_obj
+			while(temp_date <= d_to_obj):
+				val = ''
+				val = (str(temp_date.strftime("%a")) + ' ' +
+					   str(temp_date.strftime("%b")) + ' ' +
+					   str(temp_date.strftime("%d")))
+				summary_header_list.append(val)
+				date_range_list.append(temp_date.strftime
+									   (DEFAULT_SERVER_DATETIME_FORMAT))
+				temp_date = temp_date + datetime.timedelta(days=1)
+			all_detail.append(summary_header_list)
+			room_ids = room_obj.search([])
+			all_room_detail = []
+			for room in room_ids:
+				room_detail = {}
+				room_list_stats = []
+				room_detail.update({'name': room.name or ''})
 				# Include a check for bed_reservation_line_ids
-                if not room.room_reservation_line_ids and \
-                   not room.room_line_ids and not room.bed_reservation_line_ids:
-                    for chk_date in date_range_list:
-                        room_list_stats.append({'state': 'Free',
-                                                'date': chk_date})
-                else:
-                    for chk_date in date_range_list:
-                        reserline_ids = room.room_reservation_line_ids.ids
-                        reservline_ids = (reservation_line_obj.search
-                                          ([('id', 'in', reserline_ids),
-                                            ('check_in', '<=', chk_date),
-                                            ('check_out', '>=', chk_date),
-                                            ('status', '!=', 'cancel')
-                                            ]))
-                        fol_room_line_ids = room.room_line_ids.ids
-                        chk_state = ['draft', 'cancel']
-                        folio_resrv_ids = (folio_room_line_obj.search
-                                           ([('id', 'in', fol_room_line_ids),
-                                             ('check_in', '<=', chk_date),
-                                             ('check_out', '>=', chk_date),
-                                             ('status', 'not in', chk_state)
-                                             ]))
+				if not room.room_reservation_line_ids and \
+				   not room.room_line_ids and not room.bed_reservation_line_ids:
+					for chk_date in date_range_list:
+						room_list_stats.append({'state': 'Free',
+												'date': chk_date})
+				else:
+					for chk_date in date_range_list:
+						reserline_ids = room.room_reservation_line_ids.ids
+						reservline_ids = (reservation_line_obj.search
+										  ([('id', 'in', reserline_ids),
+											('check_in', '<=', chk_date),
+											('check_out', '>=', chk_date),
+											('status', '!=', 'cancel')
+											]))
+						fol_room_line_ids = room.room_line_ids.ids
+						chk_state = ['draft', 'cancel']
+						folio_resrv_ids = (folio_room_line_obj.search
+										   ([('id', 'in', fol_room_line_ids),
+											 ('check_in', '<=', chk_date),
+											 ('check_out', '>=', chk_date),
+											 ('status', 'not in', chk_state)
+											 ]))
 						# Make a check for bed_reservation_line_ids in case of dorm-room
 						# If number of bed reservations is equal to the room capacity -> the room is occupied
 						dorm_occupied = 0
 						if room.dormitory:
 							bed_res_ids = room.bed_reservation_line_ids.ids
 							bed_reservations = (reservation_line_obj.search
-                                          ([('id', 'in', bed_res_ids),
-                                            ('check_in', '<=', chk_date),
-                                            ('check_out', '>=', chk_date),
-                                            ('status', '!=', 'cancel')
-                                            ]))
+										  ([('id', 'in', bed_res_ids),
+											('check_in', '<=', chk_date),
+											('check_out', '>=', chk_date),
+											('status', '!=', 'cancel')
+											]))
 							print bed_res
 							if room.capacity == len(bed_reservations):
 								dorm_occupied = 1
 						# Include a check for the dorm_occupied-flag
-                        if reservline_ids or folio_resrv_ids or dorm_occupied:
-                            room_list_stats.append({'state': 'Reserved',
-                                                    'date': chk_date,
-                                                    'room_id': room.id,
-                                                    'is_draft': 'No',
-                                                    'data_model': '',
-                                                    'data_id': 0})
-                        else:
-                            room_list_stats.append({'state': 'Free',
-                                                    'date': chk_date,
-                                                    'room_id': room.id})
+						if reservline_ids or folio_resrv_ids or dorm_occupied:
+							room_list_stats.append({'state': 'Reserved',
+													'date': chk_date,
+													'room_id': room.id,
+													'is_draft': 'No',
+													'data_model': '',
+													'data_id': 0})
+						else:
+							room_list_stats.append({'state': 'Free',
+													'date': chk_date,
+													'room_id': room.id})
 
-                room_detail.update({'value': room_list_stats})
-                all_room_detail.append(room_detail)
-            main_header.append({'header': summary_header_list})
-            self.summary_header = str(main_header)
-            self.room_summary = str(all_room_detail)
-        return res
+				room_detail.update({'value': room_list_stats})
+				all_room_detail.append(room_detail)
+			main_header.append({'header': summary_header_list})
+			self.summary_header = str(main_header)
+			self.room_summary = str(all_room_detail)
+		return res
